dfdf6428ac539f9de20672a67c50f624f306f13b
Abraham <abdiaz2018@gmail.com>
2021-01-28 17:28:45 -0800

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,10 @@
-# your code goes here
+require 'pry'
+class Person
+    attr_reader :name
+    attr_accessor :bank_account, :happiness
+    def initialize(name)
+        @name = name
+        @bank_account = 25
+        @happiness = (8)
+    end
+end
