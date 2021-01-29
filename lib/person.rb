fe1da9458f6debbb9ee17cd36d431dfef7007bd2
Abraham <abdiaz2018@gmail.com>
2021-01-28 17:38:08 -0800

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,10 +1,12 @@
 require 'pry'
 class Person
     attr_reader :name
-    attr_accessor :bank_account, :happiness
+    attr_accessor :bank_account
     def initialize(name)
         @name = name
         @bank_account = 25
         @happiness = (8)
+    end
+    def happiness(0...10)
     end
 end
